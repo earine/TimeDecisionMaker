61e26f740eb279e67504c5b1a862ae5f5f898cd1
earine <marinalunts@gmail.com>
2019-05-08 22:38:10 +0300

    updated Appointment model — added time converter from string, event's status; updated service file — added method for getting variables from .ics file

--- TimeDecisionMaker/Model/Appointment.swift
+++ TimeDecisionMaker/Model/Appointment.swift
@@ -9,19 +9,87 @@ import Foundation
 
 class Appointment : NSObject {
     
-    public var summary : String = ""
-    public var location : String = ""
- 
-    func isReadyToAdd() -> Bool {
-        return summary.count > 0  && location.count > 0  
-    }
+    public var summary : String
+    public var created : Date!
+    public var UID : String
+    public var descriptionAp : String?
+    public var status : Status!
+    public var dateStart : Date!
+    public var dateEnd : Date!
+    private let dateFormatter = DateFormatter()
     
     override init() {
         self.summary = ""
-        self.location  = ""
+        self.UID = ""
+        self.created = Date()
+        self.status = Status.UNSET
     }
-     init(summary: String, location: String) {
+    
+    public init(summary: String, created: Date, UID: String, status: Status, description: String, dateStart: Date, dateEnd: Date) {
         self.summary = summary
-        self.location = location
+        self.UID = UID
+        self.status = status
+        self.descriptionAp = description
+        self.created = created
+        self.dateStart = dateStart
+        self.dateEnd = dateEnd
+    }
+    
+    func isReadyToAdd() -> Bool {
+        return !(summary.isEmpty || summary == "" || UID.isEmpty || UID == "" && status == Status.UNSET)
+    }
+    
+    public func typeFromString(value: String) -> Status {
+        switch value {
+        case "TENTATIVE":
+            return Status.TENTATIVE
+        case "CONFIRMED":
+            return Status.CONFIRMED
+        case "CANCELLED":
+            return Status.CANCELLED
+        default:
+            return Status.TENTATIVE
+        }
+    }
+    
+    public func makeModelEmptyForChecking() {
+        self.summary = ""
+        self.UID = ""
+        self.status = Status.UNSET
+    }
+    
+    public func convertStringToDate(value: String, timezone: String) -> Date {
+        dateFormatter.dateFormat = "yyyyMMdd'T'HHmmss'Z'"
+        
+        if var date = dateFormatter.date(from: value) {
+            return date.convertToTimeZone(initTimeZone:TimeZone(abbreviation: "UTC")!, timeZone: TimeZone(identifier: timezone)!)
+        } else {
+            return Date()
+        }
+    }
+    
+    public func dateToString(date: Date) -> String {
+        dateFormatter.dateFormat = "yyyy.MM.dd HH:mm"
+        return dateFormatter.string(from: date)
     }
 }
+
+extension Date {
+    func convertToTimeZone(initTimeZone: TimeZone, timeZone: TimeZone) -> Date {
+        let delta = TimeInterval(timeZone.secondsFromGMT() - initTimeZone.secondsFromGMT())
+        return addingTimeInterval(delta)
+    }
+}
+
+enum Status : String {
+    
+    case TENTATIVE = "TENTATIVE"
+    case CONFIRMED = "CONFIRMED"
+    case CANCELLED = "CANCELLED"
+    case UNSET
+    
+    var description: String {
+        return self.rawValue
+    }
+}
+
--- TimeDecisionMaker/Service.swift
+++ TimeDecisionMaker/Service.swift
@@ -9,22 +9,27 @@ import UIKit
 
 class Service: NSObject {
     
-    var fileKeys = ["SUMMARY:", "CREATED:"]
-    var serviceAppointment = Appointment()
+    var fileKeys = ["SUMMARY:", "CREATED:", "STATUS:", "DESCRIPTION:", "UID:", "DTSTART:", "DTEND:"]
+    var timezone: String!
     var appointments = [Appointment]()
     
     public func fetchAppointment(resourceFile: String) -> [Appointment] {
-        
+       appointments.removeAll()
+        var serviceAppointment = Appointment()
         if let path = Bundle.main.path(forResource: resourceFile, ofType: "ics") {
             do {
                 let myStrings = try String(contentsOfFile: path, encoding: String.Encoding.utf8).components(separatedBy: .newlines)
                 var state = false
+                
                 for element in myStrings {
+                    
                     if element == "BEGIN:VEVENT" {
                         state = true
                     } else if element == "END:VEVENT" {
                         state = false
-                        serviceAppointment.summary = ""
+                        serviceAppointment.makeModelEmptyForChecking()
+                    } else if element.contains("X-WR-TIMEZONE:") {
+                        timezone = element.matchingStrings(regex: "(?<=X-WR-TIMEZONE:).*").first?[0] 
                     }
                     if state {
                         if getElementByKey(element: element).1 != nil {
@@ -32,12 +37,10 @@ class Service: NSObject {
                             getVariableByKey(key: getElementByKey(element: element).0!, keyValue: getElementByKey(element: element).1!, thisAppointment: serviceAppointment)
                             
                             if serviceAppointment.isReadyToAdd() {
-                                appointments.append(Appointment(summary: serviceAppointment.summary, location: serviceAppointment.location))
+                                appointments.append(Appointment(summary: serviceAppointment.summary, created: serviceAppointment.created, UID: serviceAppointment.UID, status: serviceAppointment.status, description: serviceAppointment.description, dateStart: serviceAppointment.dateStart, dateEnd: serviceAppointment.dateEnd))
                             }
                         }
-                        
                     }
-
                 }
             } catch {
                 print("Failed to read text")
@@ -45,10 +48,10 @@ class Service: NSObject {
         } else {
             print("Failed to load file from app bundle")
         }
-
+        
         return appointments
     }
-
+    
     private func getElementByKey(element: String) -> (String?, String?) {
         var stringWithoutKeyName : String!
         var keyName : String!
@@ -63,10 +66,20 @@ class Service: NSObject {
     
     private func getVariableByKey(key: String, keyValue: String, thisAppointment: Appointment) {
         switch key {
-        case "SUMMARY:":
+        case fileKeys[0]:
             thisAppointment.summary = keyValue
-        case "CREATED:":
-            thisAppointment.location = keyValue
+        case fileKeys[1]:
+            thisAppointment.created = thisAppointment.convertStringToDate(value: keyValue, timezone: timezone)
+        case fileKeys[2]:
+            thisAppointment.status = thisAppointment.typeFromString(value: keyValue)
+        case fileKeys[3]:
+            thisAppointment.descriptionAp = keyValue
+        case fileKeys[4]:
+            thisAppointment.UID = keyValue
+        case fileKeys[5]:
+            thisAppointment.dateStart = thisAppointment.convertStringToDate(value: keyValue, timezone: timezone)
+        case fileKeys[6]:
+            thisAppointment.dateEnd = thisAppointment.convertStringToDate(value: keyValue, timezone: timezone)
         default:
             print("error")
         }
