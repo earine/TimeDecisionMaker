e5748012962b570530f7e4f2a4ccf037287ee33e
earine <marinalunts@gmail.com>
2019-05-17 23:19:24 +0300

    added editing event's duration with ui

--- TimeDecisionMaker/ViewControllers/EditViewController.swift
+++ TimeDecisionMaker/ViewControllers/EditViewController.swift
@@ -14,25 +14,32 @@ class EditViewController: UIViewController {
     
     private var formatter = DateFormatter()
     private let datePicker = UIDatePicker()
+    private let durationPicker = UIDatePicker()
     
     @IBOutlet weak var dateStartTextfield: UITextField!
     @IBOutlet weak var dateEndTextfield: UITextField!
+    @IBOutlet weak var durationLabel: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
+        
     }
-    
     
     private func updateUI() {
         formatter.dateFormat = "dd/MM/yyyy HH:mm"
         formatter.timeZone = TimeZone(abbreviation: "UTC")
+        
         dateStartTextfield.text = formatter.string(from: selectedEvent.dateInterval.start)
         dateEndTextfield.text = formatter.string(from: selectedEvent.dateInterval.end)
+        durationLabel.text = selectedEvent.durarationToString(dateInterval: selectedEvent.dateInterval)
         
         datePicker.datePickerMode = .dateAndTime
         addToolBar(dateTextfield: dateStartTextfield, datePicker: datePicker)
         addToolBar(dateTextfield: dateEndTextfield, datePicker: datePicker)
+        
+        durationPicker.datePickerMode = .countDownTimer
+        addToolBar(dateTextfield: durationLabel, datePicker: durationPicker)
     }
     
     
@@ -66,6 +73,8 @@ class EditViewController: UIViewController {
             doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(doneStartDatePicker));
         } else if dateTextfield == dateEndTextfield {
             doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(doneEndDatePicker));
+        } else if dateTextfield == durationLabel {
+            doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(doneDurationPicker));
         }
         let spaceButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
         let cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(cancelDatePicker));
@@ -77,19 +86,32 @@ class EditViewController: UIViewController {
     }
     
     @objc func doneStartDatePicker(){
-        let formatter = DateFormatter()
         formatter.dateFormat = "dd/MM/yyyy HH:mm"
         
         dateStartTextfield.text = formatter.string(from: datePicker.date)
-        
+        if Service().dateViladation(startDate: Appointment().convertStringToDate(value: dateStartTextfield.text!, timezone: "Europe/Kiev", format: "dd/MM/yyyy HH:mm"), endDate: Appointment().convertStringToDate(value: dateEndTextfield.text!, timezone: "Europe/Kiev", format: "dd/MM/yyyy HH:mm")) {
+            durationLabel.text = "\(DateInterval(start: Appointment().convertStringToDate(value: dateStartTextfield.text!, timezone: "Europe/Kiev", format: "dd/MM/yyyy HH:mm"), end: Appointment().convertStringToDate(value: dateEndTextfield.text!, timezone: "Europe/Kiev", format: "dd/MM/yyyy HH:mm")).duration)"
+        } else {
+            durationLabel.text = "0h"
+        }
         self.view.endEditing(true)
     }
     
     @objc func doneEndDatePicker(){
-        let formatter = DateFormatter()
         formatter.dateFormat = "dd/MM/yyyy HH:mm"
         dateEndTextfield.text = formatter.string(from: datePicker.date)
+        if Service().dateViladation(startDate: Appointment().convertStringToDate(value: dateStartTextfield.text!, timezone: "Europe/Kiev", format: "dd/MM/yyyy HH:mm"), endDate: Appointment().convertStringToDate(value: dateEndTextfield.text!, timezone: "Europe/Kiev", format: "dd/MM/yyyy HH:mm")) {
+            durationLabel.text = "\(selectedEvent.durarationToString(dateInterval: DateInterval(start: Appointment().convertStringToDate(value: dateStartTextfield.text!, timezone: "Europe/Kiev", format: "dd/MM/yyyy HH:mm"), end: Appointment().convertStringToDate(value: dateEndTextfield.text!, timezone: "Europe/Kiev", format: "dd/MM/yyyy HH:mm"))))"
+        } else {
+            durationLabel.text = "0h"
+        }
         self.view.endEditing(true)
+    }
+    
+    @objc func doneDurationPicker() {
+        formatter.dateFormat = "dd/MM/yyyy HH:mm"
+        durationLabel.text = selectedEvent.durarationToString(dateInterval: DateInterval(start: Appointment().convertStringToDate(value: dateStartTextfield.text!, timezone: "Europe/Kiev", format: "dd/MM/yyyy HH:mm"), duration: durationPicker.countDownDuration))
+        dateEndTextfield.text = formatter.string(from: DateInterval(start: Appointment().convertStringToDate(value: dateStartTextfield.text!, timezone: "Europe/Kiev", format: "dd/MM/yyyy HH:mm"), duration: durationPicker.countDownDuration).end)
     }
     
     @objc func cancelDatePicker(){
